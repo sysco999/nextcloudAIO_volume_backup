e443931cc3b7801d1fb3dbd83b8002f19376553e
sysco999 <158301388+sysco999@users.noreply.github.com>
2024-02-24 10:21:48 +0100

    Update backup_nextcloud.sh

--- backup_nextcloud.sh
+++ backup_nextcloud.sh
@@ -2,7 +2,7 @@
 
 # Define source directories and destination path
 SOURCES=("/var/lib/docker/volumes/nextcloud_aio_nextcloud" "/var/lib/docker/volumes/nextcloud_aio_database" "/var/lib/docker/volumes/nextcloud_aio_database_dump" "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data" "/var/lib/docker/volumes/nextcloud_aio_apache" "/var/lib/docker/volumes/nextcloud_aio_mastercontainer" "/var/lib/docker/volumes/nextcloud_aio_redis" "/var/lib/docker/volumes/metadata.db")
-DESTINATION="/mnt/data/nextcloud_backup"
+DESTINATION="/sdb/data/nextcloud_backup"
 
 # Create the destination directory if it doesn't exist
 mkdir -p $DESTINATION
