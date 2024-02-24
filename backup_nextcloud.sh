47d75022dc9b3baf9ac1bd6a3acd089e9917f354
sysco999 <158301388+sysco999@users.noreply.github.com>
2024-02-24 10:19:45 +0100

    Create backup_nextcloud.sh

---
+++ backup_nextcloud.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define source directories and destination path
+SOURCES=("/var/lib/docker/volumes/nextcloud_aio_nextcloud" "/var/lib/docker/volumes/nextcloud_aio_database" "/var/lib/docker/volumes/nextcloud_aio_database_dump" "/var/lib/docker/volumes/nextcloud_aio_nextcloud_data" "/var/lib/docker/volumes/nextcloud_aio_apache" "/var/lib/docker/volumes/nextcloud_aio_mastercontainer" "/var/lib/docker/volumes/nextcloud_aio_redis" "/var/lib/docker/volumes/metadata.db")
+DESTINATION="/mnt/data/nextcloud_backup"
+
+# Create the destination directory if it doesn't exist
+mkdir -p $DESTINATION
+
+# Loop through each source directory and back it up to the destination
+for SOURCE in "${SOURCES[@]}"
+do
+    # Run rsync to perform an incremental backup
+    rsync -av --delete $SOURCE/ $DESTINATION
+done
